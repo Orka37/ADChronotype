b834b4f191187996789073bedc2a33972a2e6454
Orka37 <arka.samatham@gmail.com>
2026-01-31 14:11:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ def norm_state():
         "age": 40,
         "bmi": 22.00,
         "ethnicity": "South Asian",
-        "answer": 0,
         "predict": False,
         "predict_normal": False
     }
@@ -78,32 +77,14 @@ if st.session_state.page=="home":
 if st.session_state.page=="input":
     st.markdown("<h1 style='text-align: center;'>Input Info</h1>", unsafe_allow_html=True)
     #---Input Values---#
-    with st.form("input"):
-        chronotype_options=["Definite Morning","Moderate Morning","Intermediate","Moderate Evening","Definite Evening"]
-        chronotype=st.selectbox("**What is your sleep chronotype?**",chronotype_options,index=chronotype_options.index(st.session_state.chronotype))
-        sleeptime=st.number_input("How long do you sleep for? (hrs)",min_value=0,max_value=24,step=1,value=int(st.session_state.sleeptime))
-        age=st.number_input("How old are you? (years)",min_value=40,max_value=60,step=1,value=int(st.session_state.age))
-        bmi=st.number_input("What is your BMI?",min_value=6.7,max_value=100.0,step=0.1,value=float(st.session_state.bmi))
-        ethnicity_options=["Caucasian", "South Asian", "East Asian", "Hispanic", "African American", "Native American", "Other"]
-        ethnicity=st.selectbox("**What is your ethnicity?**",ethnicity_options,index=ethnicity_options.index(st.session_state.ethnicity))
-        submit=st.form_submit_button("Save Changes")
-    #---Submit Values---#
-    if submit:
-        st.session_state.chronotype=chronotype
-        st.session_state.sleeptime=sleeptime
-        st.session_state.age=age
-        st.session_state.bmi=round(bmi,2)
-        st.session_state.ethnicity=ethnicity
-        #---Verify Everything Answered---#
-        if st.session_state.answer==0:
-            st.markdown("<span style='color: green; font-weight: bold;'>Answers Successfully Saved!</span>", unsafe_allow_html=True)
-            st.session_state.answer=1
-        elif st.session_state.answer==1:
-            st.markdown("<span style='color: blue; font-weight: bold;'>Answers Successfully Saved!</span>", unsafe_allow_html=True)
-            st.session_state.answer=2
-        elif st.session_state.answer==2:
-            st.markdown("<span style='color: red; font-weight: bold;'>Answers Successfully Saved!</span>", unsafe_allow_html=True)
-            st.session_state.answer=0
+    chronotype_options=["Definite Morning","Moderate Morning","Intermediate","Moderate Evening","Definite Evening"]
+    st.session_state.chronotype=st.selectbox("**What is your sleep chronotype?**",chronotype_options,index=chronotype_options.index(st.session_state.chronotype))
+    st.session_state.sleeptime=st.number_input("How long do you sleep for? (hrs)",min_value=0,max_value=24,step=1,value=int(st.session_state.sleeptime))
+    st.session_state.age=st.number_input("How old are you? (years)",min_value=40,max_value=60,step=1,value=int(st.session_state.age))
+    st.session_state.bmi=st.number_input("What is your BMI?",min_value=6.7,max_value=100.0,step=0.1,value=float(st.session_state.bmi))
+    ethnicity_options=["Caucasian", "South Asian", "East Asian", "Hispanic", "African American", "Native American", "Other"]
+    st.session_state.ethnicity=st.selectbox("**What is your ethnicity?**",ethnicity_options,index=ethnicity_options.index(st.session_state.ethnicity))
+    st.rerun()
     col1, col2, col3=st.columns([1,5,1])
     with col1:
         if st.button("**Exit**"):
