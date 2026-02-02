62353843deae6004f8cec82c115e6756776c5b01
Orka37 <arka.samatham@gmail.com>
2026-02-02 11:39:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -124,10 +124,10 @@ def predict_normal():
 
 if st.session_state.page=="home":
     st.markdown("<h1 style='text-align: center;'>ADChronotype</h1>", unsafe_allow_html=True)
-    col1, col2, col3 = st.columns([1,1,1])
+    col1, col2 = st.columns([1.8,1])
     with col1:
         st.markdown("<h4 style='text-align: center;'>Alzheimer's Risk Prediction Platform - </h4>", unsafe_allow_html=True)
-    with col3:
+    with col2:
         if st.button("Learn More"):
             project_details()
     if st.session_state.predict:
@@ -172,6 +172,7 @@ if st.session_state.page == "prediction":
         st.info("This prediction is based on your age, BMI, and sleep patterns.")
     if st.button("← Return Home"):
         go("home")
+
 
 
 
