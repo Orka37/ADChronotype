3aa6ecd8b74aaec12d1c95f7af7bf0bd38ff47b4
Orka37 <arka.samatham@gmail.com>
2026-02-06 17:57:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -270,10 +270,10 @@ if st.session_state.page == "prediction":
     save()
     st.success("Results saved to your profile!")
     st.title("Results!")
-    col1, col2, = st.columns(2)
-    with col1:
-        st.metric(label="Alzheimer's Likelihood Score", value="67%", delta="Moderate Risk")
+    col1, col2, col3, col4 = st.columns([1,2,2,1])
     with col2:
-        st.info("This prediction is based on your sleep information, age, BMI, and ethnicity.")
+        st.metric(label="Alzheimer's Likelihood Score", value="67%", delta="Moderate Risk"
         if st.button("← Return Home"):
             go("home")
+    with col3:
+        st.info("This prediction is based on your sleep information, age, BMI, and ethnicity.")
