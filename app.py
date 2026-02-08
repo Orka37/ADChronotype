2d516449c9fde8422dd33e8fb0b5e605bbdcb5df
Orka37 <arka.samatham@gmail.com>
2026-02-07 22:08:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -263,11 +263,11 @@ def predict_normal():
 if st.session_state.page=="home":
     st.markdown('<div class="main-card">', unsafe_allow_html=True) 
     st.markdown('<h1 class="title-text">ADChronotype</h1>', unsafe_allow_html=True)
-    st.markdown('<p style="color:#888; margin-top:-10px; font-weight:500;">AI-DRIVEN COGNITIVE PROFILING</p>', unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: right;'>Alzheimer's Risk Prediction Platform&nbsp;&nbsp; →</h4>", unsafe_allow_html=True)
     st.markdown('</div>', unsafe_allow_html=True)
     col1, col2, col3, col4 = st.columns([0.7,9,4,1])
     with col2:
-        st.markdown("<h4 style='text-align: right;'>Alzheimer's Risk Prediction Platform&nbsp;&nbsp; →</h4>", unsafe_allow_html=True)
+        st.markdown('<p style="color:#888; margin-top:-10px; font-weight:500;">AI-DRIVEN COGNITIVE PROFILING</p>', unsafe_allow_html=True)
     with col3:
         if st.button("Click for more info!"):
             project_details()
@@ -348,6 +348,7 @@ if st.session_state.page == "prediction":
             factor_metric("Age", 1)
             factor_metric("BMI", 21)
             factor_metric("Ethnicity", 8)
+
 
 
 
