119dce9ea227ead1aa62fc7a7c03e3c1c44f3a6e
Orka37 <arka.samatham@gmail.com>
2026-03-06 13:38:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -465,12 +465,9 @@ if st.session_state.page=="home":
     if st.session_state.predict==2:
         st.toast("Success!", icon="✅")
         st.session_state.predict=3
-    col1, col2 = st.columns([0.7, 0.3])
-    with col1:
-        st.markdown("<h1 style='text-align: right; margin: 0;'>ADChronotype</h1>", unsafe_allow_html=True)
-    with col2:
-        if st.button("?", key="help_icon_circle", help=None):
-            project_details()
+    st.markdown("<h1 style='text-align: right; margin: 0;'>ADChronotype</h1>", unsafe_allow_html=True)
+    if st.button("?", key="help_icon_circle", help=None):
+        project_details()
     col1, col2 = st.columns(2)
     with col1:
         st.markdown("### Score")
@@ -567,6 +564,7 @@ if st.session_state.page=="tips":
     st.info("WORK IN PROGRESS!")
     if st.button("**Exit**"):
         go("home")
+
 
 
 
